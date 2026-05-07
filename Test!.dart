c41fbd5db3225325665244a5b26923812b8597d0
YasharthChaudhary761 <yasharthchaudhary766@gmail.com>
2026-05-07 20:28:34 +0530

    Updated Test!.dart

--- Test!.dart
+++ Test!.dart
@@ -1,36 +1,20 @@
-void EvenOdd(int number) {
-  if (number % 2 == 0) {
-    print("$number is even");
-  } else {
-    print("$number is odd");
-  }
-}
-
-void vote(int age) {
-  if (age >= 18) {
-    print('You can vote');
-  } else {
-    print('You can not vote');
-  }
+int add(int a, int b) {
+  int ans = a + b;
+  return ans;
 }
 
 void main() {
   String name = 'Yasharth';
-  int age = 20;
-  bool student = true;
+  String collegeNAme = 'IILM University';
   print('Name: $name');
-  print('Age: $age');
-  print('Is a student? $student');
-  for (int i = 1; i <= 3; i++) {
-    print('$name : $i');
-  }
-  int i = 0;
-  while (i <= 5) {
-    print(i);
-    i++;
-  }
-  vote(20);
-  vote(16);
-  EvenOdd(10);
-  EvenOdd(15);
+  print('College: $collegeNAme');
+
+  var sum = add(3, 5);
+  print('sum=$sum');
+
+  final int product = 3 * 5;
+  print('product=$product');
+
+  const String country = 'India';
+  print('Country:$country');
 }
